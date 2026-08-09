ce06dd51a05519ba9d123d9be92735ce395be88b
truswo <gravitymail123@gmail.com>
2026-08-09 13:05:38 -0300

    added an option to only break blocks in a specified list

--- src/client/java/me/truswo/miningLimits/mixin/client/WorldRendererMixin.java
+++ src/client/java/me/truswo/miningLimits/mixin/client/WorldRendererMixin.java
@@ -37,14 +37,18 @@ public class WorldRendererMixin {
             method = "drawBlockOutline",
             at = @At("HEAD"), cancellable = true
     )
-    private void drawBlockOutline(MatrixStack matrices, VertexConsumer vertexConsumer, double x, double y, double z, OutlineRenderState state, int color, float lineWidth, CallbackInfo CIR) {
+    private void drawBlockOutline(MatrixStack matrices, VertexConsumer vertexConsumer, double x, double y, double z, OutlineRenderState state, int color, float lineWidth, CallbackInfo ci) {
         var config = MiningLimits.CONFIG;
 
         assert world != null;
         assert client.player != null;
 
+        BlockPos blockPos = state.pos();
+
+        var block = world.getBlockState(blockPos).getBlock();
+
         if (config.showOutline && !client.player.isInCreativeMode()) {
-            BlockPos blockPos = state.pos();
+
 
             var limitedChunk = world.getWorldChunk(BlockPos.fromLong(BlockPos.asLong(config.limitedChunkX, 0, config.limitedChunkZ)));
             var posChunk = world.getWorldChunk(blockPos);
@@ -53,14 +57,15 @@ public class WorldRendererMixin {
                 if (
                         (config.hasHighHeight && blockPos.getY() > config.highHeight || config.hasLowHeight && blockPos.getY() < config.lowHeight)
                                 || (config.isChunkLimited && !posChunk.equals(limitedChunk))
+                                || (config.isBlockLimited && !config.allowedBlocks.contains(block.getTranslationKey()))
                 ) {
                     if (config.debugMode && !msg1) {
                         msg2 = false;
                         msg1 = true;
                         MiningLimits.LOGGER.info("[MiningLimits] Rendering highlight");
                     }
-                    VertexRendering.drawOutline(matrices, vertexConsumer, state.shape(), blockPos.getX() - x, blockPos.getY() - y, blockPos.getZ() - z, config.outlineColor.toInt(), 1.5f);
-                    CIR.cancel();
+                    VertexRendering.drawOutline(matrices, vertexConsumer, state.shape(), blockPos.getX() - x, blockPos.getY() - y, blockPos.getZ() - z, config.outlineColor.toInt(), 1.5F);
+                    ci.cancel();
                 }
             }
         } else if (client.player.isInCreativeMode() && config.debugMode && !msg2) {
